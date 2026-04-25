a069c57f304d8d2fe15eb4d34836e6ae1a599201
edencore01@gmail.com <edencore01@gmail.com>
2026-04-25 14:57:05 +0100

    Fix homepage route

--- todo/urls.py
+++ todo/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', home),
+    path('', views.home),
     path('todo/', views.todo, name='todo'),
     path('todo/details/<int:id>', views.details, name='details'),
     path('todo/add/', views.add_todo, name="add_todo"),
